cfc4387d424a50f00456694fd5c914ebf45d3118
Lex <akimov.infotech@gmail.com>
2016-08-09 18:08:29 +0600

    WaitAll WhenAll functions

---
+++ MainSolution/Multithreading/_9_WaitAll_WhenAll.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    class _9_WaitAll_WhenAll
+    {
+        public static void Main(string[] args)
+        {
+            Task[] tasks1 = new Task[3];
+
+            tasks1[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("1");
+                return 1;
+            });
+
+            tasks1[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("2");
+                return 2;
+            });
+
+            tasks1[2] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("3");
+                return 3;
+            });
+
+            //Task.WaitAll(tasks);
+            
+            Task.WhenAll(tasks1)
+                .ContinueWith((t) =>
+                {
+                    Console.WriteLine("All task is complete...");
+                });
+            Task<int>[] tasks = new Task<int>[3];
+
+            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
+            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
+            tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
+
+            while (tasks.Length > 0)
+            {
+                int i = Task.WaitAny(tasks);
+                Task<int> completedTask = tasks[i];
+
+                Console.WriteLine(completedTask.Result);
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
